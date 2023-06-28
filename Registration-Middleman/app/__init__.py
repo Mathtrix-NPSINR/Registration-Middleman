2e6ccee3b24c6423b06fad152ef3f09e861c1422
Harikeshav R <r.harikeshav@gmail.com>
2023-06-28 17:52:35 +0530

    Create middleman server and test with Agon

---
+++ Registration-Middleman/app/__init__.py
@@ -0,0 +1,126 @@
+import os
+
+import httpx
+from dotenv import load_dotenv
+from fastapi import APIRouter, FastAPI
+from loguru import logger
+from pydantic import BaseModel
+
+load_dotenv()
+logger.remove()
+logger.add("mathtrix.log", enqueue=True)
+
+router = APIRouter()
+
+API_URL = os.getenv("API_URL")
+API_KEY = os.getenv("API_KEY")
+
+
+form_ids = {
+    "Agon": (1, "1C3nr44s7p3VNF_WHxWkKAaunQ1QvN-WcrwiKEYvUyNY"),
+    "Apollo": (2, "10Fy406Lz0h7azBY3BYcKD82R2UnnxWCUjO4CvQwOl1k"),
+    "Auctus": (3, "1utGuIQ8pyj9EYRmlNPl4q5xVgccCQEX94c_WJ9Gizzo"),
+    "Enigmata": (4, "15qXV8Q-YLzfkWMOxbdKLkkHkEIyRJj0tpZQi8dvU3j8"),
+    "Mask A Raid": (5, "1s7om5bwTshQWPsQE3f7OkDvXvAMOuvsM8jF1Xnp50YQ"),
+    "Mercatus": (6, "1B5oP2MBaYZEjDPhw_IHroehbS7gV8hZhO79V-GaO0-Y"),
+    "Nemesis": (7, "1GxPRC5T2npL-ablix1q8Dhi5Gy-3lXZelcNhsOplmDc"),
+    "Saturn": (8, "1-Wo5IzWj26KzyQeZIR5511qELXnVw5ObJMJt8JXC1DU"),
+    "Sonneto": (9, "19AiL7fX8DQBR4NaHoHARo0k40xeWSPeRUq1__HIoYM0"),
+    "Sum it Up!": (10, "1ym1ce1LbFMHfFTiweFJWHd6dzc4-NMCUj-XNQLxtqOA"),
+}
+
+
+class EventBase(BaseModel):
+    form_id: str
+    school: str
+    team_name: str
+
+    name_1: str | None = None
+    email_1: str | None = None
+    phone_number_1: str | None = None
+
+    name_2: str | None = None
+    email_2: str | None = None
+    phone_number_2: str | None = None
+
+    name_3: str | None = None
+    email_3: str | None = None
+    phone_number_3: str | None = None
+
+    name_4: str | None = None
+    email_4: str | None = None
+    phone_number_4: str | None = None
+
+    name_5: str | None = None
+    email_5: str | None = None
+    phone_number_5: str | None = None
+
+    name_6: str | None = None
+    email_6: str | None = None
+    phone_number_6: str | None = None
+
+    name_7: str | None = None
+    email_7: str | None = None
+    phone_number_7: str | None = None
+
+
+@router.post("/")
+async def forward(*, event: EventBase):
+    for event_name, details in form_ids.items():
+        if event.form_id == details[1]:
+            event_id = details[0]
+            event_name = event_name
+            break
+
+    async with httpx.AsyncClient(timeout=None) as client:
+        team = await client.post(
+            f"{API_URL}/team/",
+            params={"api-key": API_KEY},
+            json={
+                "team_name": event.team_name,
+                "team_school": event.school,
+                "team_event": event_name,
+                "event_id": event_id,
+            },
+        )
+
+        team_data = team.json()
+        team_id = team_data["id"]
+
+        if (
+            event.name_1 is not None
+            and event.email_1 is not None
+            and event.phone_number_1 is not None
+        ):
+            await client.post(
+                f"{API_URL}/user/",
+                params={"api-key": API_KEY},
+                json={
+                    "user_name": event.name_1,
+                    "user_email": event.email_1,
+                    "user_phone": event.phone_number_1,
+                    "user_school": event.school,
+                    "team_id": team_id,
+                },
+            )
+
+        if (
+            event.name_2 is not None
+            and event.email_2 is not None
+            and event.phone_number_2 is not None
+        ):
+            await client.post(
+                f"{API_URL}/user/",
+                params={"api-key": API_KEY},
+                json={
+                    "user_name": event.name_2,
+                    "user_email": event.email_2,
+                    "user_phone": event.phone_number_2,
+                    "user_school": event.school,
+                    "team_id": team_id,
+                },
+            )
+
+
+app = FastAPI(title="Registration MiddleMan")
+app.include_router(router)
